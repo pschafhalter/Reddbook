b6e9f92866cd5c185d9ca7beef5d7e8e0785a69f
Peter Schafhalter <pschafhalter@berkeley.edu>
2015-12-18 18:21:13 -0800

    First working version

--- replace.js
+++ replace.js
@@ -1,6 +1,34 @@
+function getFrontPage(callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+	if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+	    callback(JSON.parse(xmlHttp.responseText));
+    }
+    xmlHttp.open("GET", "https://www.reddit.com/.json", true);
+    xmlHttp.send(null);
+}
+
+
 setTimeout(function() {
     var streamContainer = document.getElementById("stream_pagelet");
     while(streamContainer.children.length) {
 	streamContainer.removeChild(streamContainer.children[0])
     }
+
+    var addReddit = function(obj) {
+	var posts = obj.data.children;
+	console.log(posts);
+	for(var i = 0; i < posts.length; i++) {
+	    post = posts[i].data;
+	    d = document.createElement("div");
+	    link = document.createElement("a");
+	    link.setAttribute("href", post.url);
+	    title = document.createTextNode(post.title);
+	    link.appendChild(title);
+	    d.appendChild(link);
+	    streamContainer.appendChild(d);
+	}
+    }
+    getFrontPage(addReddit); 
 }, 1000);
+
