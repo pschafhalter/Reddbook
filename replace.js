8aaeceb3a4c0067d1fd1aec0167424bbca9bafa5
Peter Schafhalter <pschafhalter@berkeley.edu>
2015-12-18 21:47:36 -0800

    Added Facebook-style backgrounds

--- replace.js
+++ replace.js
@@ -17,16 +17,35 @@ setTimeout(function() {
 
     var addReddit = function(obj) {
 	var posts = obj.data.children;
-	console.log(posts);
-	for(var i = 0; i < posts.length; i++) {
-	    post = posts[i].data;
-	    d = document.createElement("div");
-	    link = document.createElement("a");
+	/*for(var i = 0; i < posts.length; i++) {
+	    var post = posts[i].data;
+	    var d = document.createElement("div");
+	    var link = document.createElement("a");
 	    link.setAttribute("href", post.url);
-	    title = document.createTextNode(post.title);
+	    var title = document.createTextNode(post.title);
 	    link.appendChild(title);
 	    d.appendChild(link);
 	    streamContainer.appendChild(d);
+	}*/
+	for(var i = 0; i < posts.length; i++) {
+	    var post = posts[i].data;
+	    var outer = document.createElement("div");
+	    outer.className = "_4-u2 mbm _5v3q _2l4l _4-u8";
+	    var inner = document.createElement("div");
+	    inner.className = "_3ccb";
+	    var wrapper = document.createElement("div");
+	    wrapper.className = "userContentWrapper _5pcr";
+	    var content = document.createElement("div");
+	    content.className = "_5pbx userContent";
+	    link = document.createElement("a");
+	    link.setAttribute("href", post.url);
+	    t = document.createTextNode(post.title);
+	    link.appendChild(t);
+	    content.appendChild(link);
+	    wrapper.appendChild(content);
+	    inner.appendChild(wrapper);
+	    outer.appendChild(inner);
+	    streamContainer.appendChild(outer);
 	}
     }
     getFrontPage(addReddit); 
